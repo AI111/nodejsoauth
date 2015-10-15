c6b54f9f20de13ee8731558da3a44702416025f1
sasha <sashaandreev7@gmail.com>
2015-10-15 20:44:04 +0300

    initial commit

--- config/index.js
+++ config/index.js
@@ -3,22 +3,13 @@
  */
 'use strict';
 
-var connection_string = '127.0.0.1:27017/nodejsoauth';
-// if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
+
 module.exports = {
-    // MongoDB connection options
+
     ip:       process.env.OPENSHIFT_NODEJS_IP ||
     process.env.IP ||
     undefined,
 
-    // Server port
     port:     process.env.OPENSHIFT_NODEJS_PORT ||
     process.env.PORT ||
     8080,
@@ -27,11 +18,7 @@ module.exports = {
 
     // MongoDB connection options
     mongo: {
-        connection_string:connection_string,
-        uri:    (process.env.OPENSHIFT_MONGODB_DB_HOST || 'mongodb://localhost')+"/nodejsoauth",
-        options: {
-        user: process.env.OPENSHIFT_MONGODB_DB_USERNAME||undefined,
-        pass: process.env.OPENSHIFT_MONGODB_DB_PASSWORD||undefined
-    }
+        connection_string:process.env.OPENSHIFT_MONGODB_DB_URL||'127.0.0.1:27017/nodejsoauth'
+
     }
 };
